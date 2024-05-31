394440d09526417f0701aba03c0e53f2f33c39ca
Billa05 <workinguse5@gmail.com>
2024-05-31 18:31:01 +0530

    fastapi route

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+@app.post("/info")
+async def label_info(ingredients,nutritional):
+    # SEND THIS INFO TO THE QUERY FORMAT
+
+@app.get("/info")
+async def get_info():
+    # GET THE REALITY OF THE PRODUCT
+
+@app.post("/company")
+async def claim(company_claim,product_reality):
+    # send this to the query format
+
+@app.get("/company")
+async def get_claim():
+    # return claim vs reality
+
+
+
+
