bc304921be920510589899ca6273aa9455ae1b5c
Billa05 <workinguse5@gmail.com>
2024-06-04 17:41:08 +0530

    added all the data

--- backend/first_chain.py
+++ backend/first_chain.py
@@ -1,12 +1,9 @@
 import os
 from langchain_groq import ChatGroq
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
-from langchain_community.vectorstores import FAISS
-from langchain_community.document_loaders import PyPDFDirectoryLoader
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from main import vectors
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -29,16 +26,6 @@ Questions:{input}
 
 """
 )
-
-
-
-embeddings=GoogleGenerativeAIEmbeddings(model = "models/embedding-004")
-loader=PyPDFDirectoryLoader("./data") ## Data Ingestion
-docs=loader.load() ## Document Loading
-text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200) ## Chunk Creation
-final_documents=text_splitter.split_documents(docs[:20]) #splitting
-vectors=FAISS.from_documents(final_documents,embeddings) #vector OpenAI embeddings
-
 
 
 document_chain=create_stuff_documents_chain(llm,prompt)
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from vector_embedding import create_vectorDB
 from models import Ingredients,Nutritional,Product_reality
 app = FastAPI()
 
+async def startup_event():
+    global vectors
+    directory = "./data"
+    vectors = create_vectorDB(directory)
+    print("Vector Store DB is ready")
+
+app.add_event_handler("startup", startup_event)
 
 @app.post("/info")
 async def label_info(ingredients :Ingredients,nutritional:Nutritional):
