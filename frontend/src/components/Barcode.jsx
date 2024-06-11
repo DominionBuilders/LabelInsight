5630939e765088112ab1cef9f57f85c0dbf44edd
Billa05 <workinguse5@gmail.com>
2024-06-11 23:20:08 +0530

    trial 3

--- frontend/src/components/Barcode.jsx
+++ frontend/src/components/Barcode.jsx
@@ -1,38 +1,60 @@
-import React, { useRef, useEffect, useState } from 'react';
 import { BrowserMultiFormatReader } from '@zxing/library';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
-export default function Barcode() {
-  const videoRef = useRef(null);
-  const reader = useRef(new BrowserMultiFormatReader());
-  const [result, setResult] = useState("null"); 
+const useZxing = ({
+  constraints = {
+    audio: false,
+    video: {
+      facingMode: 'environment',
+    },
+  },
+  hints,
+  timeBetweenDecodingAttempts = 300,
+  onResult = () => {},
+  onError = () => {},
+} = {}) => {
+  const ref = useRef(null);
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
+
+  const reader = useMemo(() => {
+    const instance = new BrowserMultiFormatReader(hints);
+    instance.timeBetweenDecodingAttempts = timeBetweenDecodingAttempts;
+    return instance;
+  }, [hints, timeBetweenDecodingAttempts]);
 
   useEffect(() => {
-    if (!videoRef.current) return;
-    reader.current.decodeFromConstraints(
-      {
-        audio: false,
-        video: {
-          facingMode: 'environment',
-        },
-      },
-      videoRef.current,
-      (result, error) => {
-        if (result) {
-          console.log(result);
-          setResult(result.text);
-        }
-        if (error) console.log(error);
+    if (!ref.current) return;
+    reader.decodeFromConstraints(constraints, ref.current, (result, err) => {
+      if (result) {
+        setResult(result);
+        onResult(result);
       }
-    );
+      if (err) {
+        setError(err);
+        onError(err);
+      }
+    });
     return () => {
-      reader.current.reset();
+      reader.reset();
     };
-  }, [videoRef]);
+  }, [ref, reader]);
 
+  return { ref, result, error };
+};
+
+const Barcode = ({
+  onResult = () => {},
+  onError = () => {},
+}) => {
+  const { ref, result, error } = useZxing({ onResult, onError });
   return (
-    <>
-      <video ref={videoRef} />
-      <p>{result}</p> 
-    </>
+  <>
+    <video ref={ref} />
+    {result && <p>Result: {result.text}</p>}
+    {error && <p>Error: {error.message}</p>}
+  </>
   );
-}
+};
+
+export default Barcode;
