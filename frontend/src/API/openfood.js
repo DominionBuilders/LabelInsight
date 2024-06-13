d6ed590ee1ef94c02cebd0e27092de07bc41fa81
Billa05 <workinguse5@gmail.com>
2024-06-13 13:16:37 +0530

    debugging

--- frontend/src/API/openfood.js
+++ frontend/src/API/openfood.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default function api_call(EAN, setResponse) {
     axios.get(`https://world.openfoodfacts.net/api/v2/product/${EAN}`)
         .then(response => {
-            setResponse(response.data);
+            setResponse(response.data.status_verbose);
         })
         .catch(error => {
             setResponse(error.message);
