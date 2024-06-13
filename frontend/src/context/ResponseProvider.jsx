59c067b72f63a09a24790c23ce75edc153c3269c
Billa05 <workinguse5@gmail.com>
2024-06-13 12:18:51 +0530

    bug fix

--- frontend/src/context/ResponseProvider.jsx
+++ frontend/src/context/ResponseProvider.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { ResponseContext } from './ResponseContext';
 
 export const ResponseProvider = ({ children }) => {
-    const [response, setResponse] = useState(null);
+    const [Response, setResponse] = useState(null);
 
     return (
         <ResponseContext.Provider value={{ Response, setResponse }}>
