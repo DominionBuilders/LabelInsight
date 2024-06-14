79efb59f6ed14481ee312fb323abdce25cb25bef
Biresh Biswas <90760974+Billa05@users.noreply.github.com>
2024-06-14 15:11:01 +0530

    Update openfood.js

--- frontend/src/API/openfood.js
+++ frontend/src/API/openfood.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default function api_call(EAN, setResponse) {
     axios.get(`https://world.openfoodfacts.net/api/v2/product/${EAN}`)
         .then(response => {
-            setResponse(response.data.product.product_name);
+            setResponse(response.data.product.popularity_tags.product_name);
         })
         .catch(error => {
             setResponse(error.message);
